b6d9118ee98b85cd02ece9619465aad36264910e
ANDreew1987 <andreew.12.87@gmail.com>
2021-01-18 22:45:23 +0700

    Add comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,15 +17,22 @@ public class Player : MonoBehaviour
     private System.Random _random = new System.Random();
 
     private Rigidbody2D _body;
+    // Сенсоры
     private Sensor[] _sensors;
+    // Сенсор для определения того, что находимся на земле
     private GroundSensor _groundSensor;
 
+    // Перемещение
     private double _move;
+    // Прыжок
     private double _jump;
     
+    // Разворот влево
     private bool _facingLeft = false;
 
+    // Жив ли игрок
     private bool _alive = false;
+    // Таймер "жизни" игрока
     private Timer _aliveTimer;
 
     private bool _initialized = false;
@@ -38,26 +45,37 @@ public class Player : MonoBehaviour
         _body = GetComponent<Rigidbody2D>();
     }
 
+    // Получаем значения сенсоров
     private double[] GetSensorsOutput()
     {
+        // Массив с результатами
         double[] sensorsOutput = new double[_sensors.Length];
         for (int i = 0; i < _sensors.Length; i++)
         {
+            // Запрашиваем значение сенсора
             sensorsOutput[i] = _sensors[i].Output;
         }
         return sensorsOutput;
     }
 
+    // Обновляем значения переменных управления
+    // Подаём значения сенсоров
     private void UpdateControlValues(double[] sensorsOutput)
     {
+        // Прогоняем значения сенсоров через нейронную сеть
         Math.Vector controls = _nn.Forward(new Math.Vector(sensorsOutput));
+        // Обновляем значения переменных управления
         _move = (controls[0] * 2 - 1) * speed;
         _jump = controls[1] * 2 - 1;
     }
 
+    // Переворот игрока влево\вправо
     private void FlipX()
     {
+        // Получаем вектор масштабирования игрока
         Vector3 localScale = transform.localScale;
+        // Если значение перемещения отрицательное,
+        // то двигаемся назад, те переворачиваем игрока (по оси х)
         if (_move < -0.01)
         {
             _facingLeft = false;
@@ -73,6 +91,8 @@ public class Player : MonoBehaviour
         transform.localScale = localScale;
     }
 
+    // Создание и инициализация нейронной сети
+    // На входе массив с количеством нейронов в скрытых слоях
     private void InitNN(uint[] nnTopology)
     {
         NN.LayerConfig[] nnConfig = new NN.LayerConfig[nnTopology.Length + 1];
@@ -88,9 +108,11 @@ public class Player : MonoBehaviour
         _nn.SetRandomWeights(-1.0, 1.0, _random);
     }
 
+    // Копирование весов в особь
     private void CopytWeightsToIndividual()
     {
         int individualPos = 0;
+        // Проходим по всем слоям, строкам, столбцам и добавляем веса в особъ
         for (int layer = 0; layer < _nn.LayersCount; layer++)
         {
             for (int row = 0; row < _nn[layer].Rows; row++)
@@ -107,16 +129,20 @@ public class Player : MonoBehaviour
     {
         if (_initialized)
         {
+            // 1. Получаем значения сенсоров
             double[] sensorsOutput = GetSensorsOutput();
+            // 2. Прогоняем значения сенсоров через нейронную сеть
+            // и обновляем значения управляющих переменных
             UpdateControlValues(sensorsOutput);
 
+            // 3. Перемещаем персонажа
             _body.velocity = new Vector2((float)_move, _body.velocity.y);
-
+            // 3.1. Прыгаем, если нужно
             if (_groundSensor.Grounded && (float)_jump > jumpThreshold)
             {
                 _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
-
+            // 4. Разворачиваем персонажа, если нужно
             FlipX();
         }
     }
@@ -129,14 +155,13 @@ public class Player : MonoBehaviour
             _groundSensor = GetComponentInChildren<GroundSensor>();
         }
 
+        // Инициализируем первое поколение
         InitNN(nnTopology);
-
+        // Создаём особь
         _individual = new GA.Individual(_nn.WeightsCount);
-
+        // Копируем веса в особь
         CopytWeightsToIndividual();
-
-        
-
+        // Настраиваем таймер, отвечающий за жизнь персонажа
         _aliveTimer = new Timer(aliveTime);
         _aliveTimer.Elapsed += OnTimedEvent;
         _aliveTimer.AutoReset = true;
@@ -146,6 +171,7 @@ public class Player : MonoBehaviour
         _initialized = true;
     }
 
+    // Жив ли персонаж
     public bool Alive
     {
         get
@@ -154,26 +180,33 @@ public class Player : MonoBehaviour
         }
         set
         {
+            // Значение не поменялось, просто выходим
             if (_alive == value) {
                 return;
             }
             _alive = value;
             if (_alive)
             {
+                // Персонаж жив
                 for (int i = 0; i < _sensors.Length; i++)
                 {
+                    // Показываем сенсоры
                     _sensors[i].Show();
                 }
                 _groundSensor.Show();
+                // Запускаем таймер
                 _aliveTimer.Start();
             }
             else
             {
+                // Персонаж умер
                 for (int i = 0; i < _sensors.Length; i++)
                 {
+                    // Скрываем сенсоры
                     _sensors[i].Hide();
                 }
                 _groundSensor.Hide();
+                // Останавливаем таймер
                 _aliveTimer.Stop();
             }
         }
@@ -191,6 +224,7 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Копируем гены из особи в веса нейронной сети
     public void FitWeights()
     {
         int individualPos = 0;
@@ -208,6 +242,7 @@ public class Player : MonoBehaviour
 
     private void OnTimedEvent(System.Object source, ElapsedEventArgs e)
     {
+        // Время жизни вышло, убиваем персонажа
         Alive = false;
     }
 
--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -21,18 +21,23 @@ public class Scene : MonoBehaviour
 
     private Camera _camera;
 
+    // Данные игрока
     private struct PlayerData
     {
+        // Игрок, как объект движка
         public GameObject playerObject;
+        // Игрок, как объект из скрипта
         public Player player;
     }
 
+    // Игроки
     private PlayerData[] _players;
     
     private GA.GeneticAlgorithm _ga;
 
     private bool _initialized = false;
 
+    // Создаём и инициализируем персонажей
     private void InitPlayers()
     {
         _players = new PlayerData[PopulationSize];
@@ -44,12 +49,14 @@ public class Scene : MonoBehaviour
         }
     }
 
+    // Сброс персонажей
     private void ResetPlayers()
     {
         for (int i = 0; i < _players.Length; i++)
         {
             if (_players[i].playerObject)
             {
+                // Игрок появляется в начальной точке
                 _players[i].playerObject.transform.position = new Vector3(-7.5f, -3f, -1f);
                 _players[i].playerObject.SetActive(true);
                 _players[i].player.Alive = true;
@@ -57,6 +64,7 @@ public class Scene : MonoBehaviour
         }
     }
 
+    // Есть ли живие персонажи
     private bool PopulationAlive()
     {
         bool alive = false;
@@ -71,29 +79,41 @@ public class Scene : MonoBehaviour
         return alive;
     }
 
+    // Обработка персонажей
     private void ProcessPlayers()
     {
         for (int i = 0; i < _players.Length; i++)
         {
+            // Если игрок провалился в пропасть
             if (_players[i].playerObject.transform.position.y < -5.0f)
             {
+                // убиваем игрока
                 _players[i].playerObject.SetActive(false);
                 _players[i].player.Alive = false;
             }
+            // Если же игрок умер по таймауту
             else if (!_players[i].player.Alive)
             {
+                // скрываем игрока ("убиваем")
                 _players[i].playerObject.SetActive(false);
             }
         }
     }
 
+    // Обработка камеры
     private void ProcessCamera()
     {
+        // Камера следит за игроком, который продвинулся дальше всех по горизонтали
+        // Выбираем живых игроков
         var alivePlayers = _players.Where(p => p.player.Alive);
+        // Если есть живые игроки
         if (alivePlayers.Count() > 0)
         {
+            // Получаем х-координату наиболее продвинувшегося игрока
             float bestX = alivePlayers.Max(p => p.playerObject.transform.position.x);
+            // Выбираем первого наиболее продвинувшегося игрока
             var best = alivePlayers.Where(p => p.playerObject.transform.position.x == bestX).First();
+            // Перемещаем камеру по х и у к игроку
             _camera.transform.position = new Vector3(
                 best.playerObject.transform.position.x,
                 best.playerObject.transform.position.y,
@@ -103,9 +123,12 @@ public class Scene : MonoBehaviour
 
     void Start()
     {
+        // Получаем камеру
         _camera = GetComponentInChildren<Camera>();
 
+        // Инизиализируем первое поколение
         InitPlayers();
+        // Перемещаем игроков к исходной точке
         ResetPlayers();
 
         _ga = new GA.GeneticAlgorithm(
@@ -121,35 +144,37 @@ public class Scene : MonoBehaviour
             return;
         }
 
+        // Обрабатываем игроков
         ProcessPlayers();
-
+        // Обрабатываем камеру
         ProcessCamera();
 
+        // Если вся популяция погибла (упала в пропасть или вышло время жизни)
         if (!PopulationAlive())
         {
+            // Получаем значение приспособленности особей как величину продвижения по оси х
             for (int p = 0; p < _players.Length; p++)
             {
                 _players[p].player.Individual.Fitness = _players[p].playerObject.transform.position.x;
             }
-
+            // Создаём популяцию
             List<GA.Individual> population = new List<GA.Individual>();
             for (int p = 0; p < _players.Length; p++)
             {
                 population.Add(_players[p].player.Individual);
             }
-
+            // Запускаем генетический алгоритм
             _ga.Run(population);
-
+            // В популяции новые значения после генетического алгоритма (новая популяция)
+            // Обновляем веса нейронных сетей игроков
             for (int p = 0; p < _players.Length; p++)
             {
                 _players[p].player.Individual = population[p];
                 _players[p].player.FitWeights();
             }
-
+            // Сброс и перемещение игроков в исходную точку
             ResetPlayers();
         }
-
     }
-
 
 }
